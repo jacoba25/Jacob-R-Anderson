8153aa9553d60cf6fa495b842fad8fc46376b7a1
Jacob Anderson <jacob99a@gmail.com>
2019-03-06 19:09:32 -0800

    Add homepage

---
+++ cmake-build-debug/CMakeFiles/helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloworld.dir/main.cpp.obj"
+  "helloworld.pdb"
+  "helloworld.exe"
+  "helloworld.exe.manifest"
+  "helloworld.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
